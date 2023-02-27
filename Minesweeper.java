1ee44a0498b2db4fee64bdb005e168b6e4df7802
23NateK <n.kimber22@gmail.com>
2023-02-27 12:31:53 -0600

    finished makeMinesweeperboard

--- Minesweeper.java
+++ Minesweeper.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+
 public class Minesweeper {
+    public static boolean[][] makeMinesweeperBoard(int n,int m, double p){
+        boolean[][] board= new boolean[n][m];
+        for(int x=0;x<board.length;x++){
+            for(int y=0;y<board[0].length;y++){
+                double rand=Math.random();
+                if(rand<p){
+                    board[x][y]=true;
+                }else{
+                    board[x][y]=false;
+                }
+
+            }
+        }
+
+
+        return board;
+    }
     public static void main(String[] args) {
-        
+    System.out.println(Arrays.deepToString(makeMinesweeperBoard(5, 5, 0.50)));
+    
     }
 }
